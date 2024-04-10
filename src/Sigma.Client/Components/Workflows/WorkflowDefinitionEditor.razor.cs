bb6e8d59102f1b17b814a5cf2e0fafd5e86e9832
James Yeung <shunjiey@hotmail.com>
2024-04-10 23:59:04 +0800

    refactor workflow list

---
+++ src/Sigma.Client/Components/Workflows/WorkflowDefinitionEditor.razor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Elsa.Studio.Workflows.Components.WorkflowDefinitionEditor;
+
+namespace Sigma.Client.Components.Workflows;
+
+public partial class WorkflowDefinitionEditor : Elsa.Studio.Workflows.Components.WorkflowDefinitionEditor.WorkflowDefinitionEditor
+{
+}
+
---
+++ src/Sigma.Client/Components/Workflows/WorkflowDefinitionList.razor.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AntDesign.TableModels;
+using Elsa.Api.Client.Shared.Models;
+using Elsa.Studio.DomInterop.Contracts;
+using Elsa.Studio.Workflows.Domain.Contracts;
+using Microsoft.AspNetCore.Components;
+using Elsa.Api.Client.Resources.WorkflowDefinitions.Requests;
+using Elsa.Api.Client.Resources.WorkflowDefinitions.Responses;
+using Elsa.Api.Client.Resources.WorkflowDefinitions.Enums;
+using Elsa.Studio.Components;
+
+namespace Sigma.Client.Components.Workflows;
+
+public partial class WorkflowDefinitionList : StudioComponentBase
+{
+    private IEnumerable<WorkflowDefinitionRow> _selectedRows = [];
+    private int _totalCount;
+
+    /// <summary>
+    /// An event that is invoked when a workflow definition is edited.
+    /// </summary>
+    [Parameter] public EventCallback<string> EditWorkflowDefinition { get; set; }
+
+    [Inject] private IWorkflowDefinitionService WorkflowDefinitionService { get; set; } = default!;
+    [Inject] private IWorkflowInstanceService WorkflowInstanceService { get; set; } = default!;
+    [Inject] private IFiles Files { get; set; } = default!;
+    [Inject] private IDomAccessor DomAccessor { get; set; } = default!;
+    private string SearchTerm { get; set; } = string.Empty;
+    private List<WorkflowDefinitionRow> _data;
+
+    private  async Task OnTableChange(QueryModel<WorkflowDefinitionRow> queryModel)
+    {
+        var request = new ListWorkflowDefinitionsRequest
+        {
+            IsSystem = false,
+            Page = queryModel.PageIndex - 1,
+            PageSize = queryModel.PageSize,
+            SearchTerm = SearchTerm,
+            OrderBy = GetOrderBy(queryModel.SortModel.FirstOrDefault()?.FieldName),
+            OrderDirection = queryModel.SortModel.FirstOrDefault()?.Sort == "descend"
+             ? OrderDirection.Descending
+             : OrderDirection.Ascending
+        };
+
+
+        _data = await base.InvokeWithBlazorServiceContext(async () =>
+        {
+            var latestWorkflowDefinitionsResponse = await WorkflowDefinitionService.ListAsync(request, VersionOptions.Latest);
+            var unpublishedWorkflowDefinitionIds = latestWorkflowDefinitionsResponse.Items.Where(x => !x.IsPublished).Select(x => x.DefinitionId).ToList();
+
+            var publishedWorkflowDefinitions = await WorkflowDefinitionService.ListAsync(new ListWorkflowDefinitionsRequest
+            {
+                DefinitionIds = unpublishedWorkflowDefinitionIds,
+            }, VersionOptions.Published);
+
+            _totalCount = (int)latestWorkflowDefinitionsResponse.TotalCount;
+
+            var latestWorkflowDefinitions = latestWorkflowDefinitionsResponse.Items
+                .Select(definition =>
+                {
+                    var latestVersionNumber = definition.Version;
+                    var isPublished = definition.IsPublished;
+                    var publishedVersion = isPublished
+                        ? definition
+                        : publishedWorkflowDefinitions.Items.FirstOrDefault(x => x.DefinitionId == definition.DefinitionId);
+                    var publishedVersionNumber = publishedVersion?.Version;
+
+                    return new WorkflowDefinitionRow(
+                        definition.Id,
+                        definition.DefinitionId,
+                        latestVersionNumber,
+                        publishedVersionNumber,
+                        definition.Name,
+                        definition.Description,
+                        definition.IsPublished);
+                })
+                .ToList();
+
+            return latestWorkflowDefinitions;
+        });
+    }
+
+    private OrderByWorkflowDefinition? GetOrderBy(string? sortLabel)
+    {
+        return sortLabel switch
+        {
+            "Name" => OrderByWorkflowDefinition.Name,
+            "Version" => OrderByWorkflowDefinition.Version,
+            "Created" => OrderByWorkflowDefinition.Created,
+            _ => null
+        };
+    }
+
+    private record WorkflowDefinitionRow(
+        string Id,
+        string DefinitionId,
+        int LatestVersion,
+        int? PublishedVersion,
+        string? Name,
+        string? Description,
+        bool IsPublished);
+}
+
+
